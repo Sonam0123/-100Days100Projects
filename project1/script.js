ea86de741f645025c04175e53513dd0fd6476380
sonam <tseringsonam34@hotmail.com>
2022-08-29 14:01:37 -0400

    project 1 completed

--- project1/script.js
+++ project1/script.js
@@ -29,19 +29,25 @@ btn.addEventListener('click', (e) => {
         }
             mealsContainer.innerHTML += `
             <div class="meal">
-                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="meal-img">
-                <h3 class="meal-title">${meal.strMeal}</h3>
-                <p class="meal-info">${meal.strInstructions}</p>
+                <div class='meal-infos'>
+                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="meal-img">
+                    <div class="meal-info">
+                        <h3 class="meal-title">${meal.strMeal}</h3>
+                        <p class="meal-info">${meal.strInstructions}</p>
+                    </div>
+                </div>
                 <p class="meal-category"><strong>Category: </strong>${meal.strCategory}</p>
                 <p class="meal-area"><strong>Area: </strong>${meal.strArea}</p>
                 <h3 class="meal-ingredients">Ingredients</h3>
                 <ul>
 					${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
 				</ul>
-                <h3 class="meal-video">Video</h3>
-                <iframe width="420" height="315"
-				src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
-				</iframe>
+                <div>
+                    <h3>Video</h3>
+                    <iframe class="video" width="420" height="315"
+                    src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+                    </iframe>
+                </div>
             </div>
 
             `
