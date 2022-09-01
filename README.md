7a930901840ac11be4c0dc847ffc5ebcbc6c30c6
sonam <tseringsonam34@hotmail.com>
2022-09-01 15:07:50 -0400

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+# 100Days100Projects
+<hr>
+<h3>Overview</h3>
+<p>
+    This is a collection of 100 projects that I have created inspired by Youtuber Florin Pop. Although I may not finish in 100 days, I will try my best to complete it within that time. This website has all the projects he challenged his viewers to complete <a href='https://www.florin-pop.com/blog/2019/09/100-days-100-projects/'>https://www.florin-pop.com/blog/2019/09/100-days-100-projects/</a>.
+</p>    
+<h3>Goals</h3>
+<p>
+    I want to complete the challenge within 100 days.My main goal is to learn as much as I can about web development on the frontend.
+</p>    
+<h3>Resources</h3>
+<p>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 563a1dd (initial commit)
+<a href='https://www.florin-pop.com/blog/2019/09/100-days-100-projects/'>https://www.florin-pop.com/blog/2019/09/100-days-100-projects/</a>.
+</p>
+<hr>
+<h2>Deployed Projects</h2>
+<break>
+<p>Project one <a href='https://sonammealgenerator.netlify.app'></a>: https://sonammealgenerator.netlify.app/</a></p>
+<<<<<<< HEAD
+=======
+=======
+    <a>https://www.florin-pop.com/blog/2019/09/100-days-100-projects/</a>
+</p>
+>>>>>>> ae99c25 (initial commit)
+>>>>>>> 563a1dd (initial commit)
---
+++ project1/script.js
@@ -0,0 +1,55 @@
+const mealsContainer = document.querySelector('.meals')
+const btn = document.querySelector('.btn')
+
+const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
+
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => 
+  
+    {   
+
+        mealsContainer.innerHTML = '';
+        data.meals.forEach((meal) => {
+                    const ingredients = [];
+
+        for (let i = 1; i <= 20; i++) {
+            if (meal[`strIngredient${i}`]) {
+                ingredients.push(
+                    `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+                );
+            } else {
+                break;
+            }
+        }
+            mealsContainer.innerHTML += `
+            <div class="meal">
+                <div class='meal-infos'>
+                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="meal-img">
+                    <div class="meal-info">
+                        <h3 class="meal-title">${meal.strMeal}</h3>
+                        <p class="meal-info">${meal.strInstructions}</p>
+                    </div>
+                </div>
+                <p class="meal-category"><strong>Category: </strong>${meal.strCategory}</p>
+                <p class="meal-area"><strong>Area: </strong>${meal.strArea}</p>
+                <h3 class="meal-ingredients">Ingredients</h3>
+                <ul>
+					${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+				</ul>
+                <div>
+                    <h3>Video</h3>
+                    <iframe class="video" width="420" height="315"
+                    src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+                    </iframe>
+                </div>
+            </div>
+
+            `
+        })    
+    })
+})
